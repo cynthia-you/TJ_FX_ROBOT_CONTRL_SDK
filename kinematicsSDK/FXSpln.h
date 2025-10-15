a602b7dee21519bbc93d462274f11e218955612a
cynthia-you <37206493+cynthia-you@users.noreply.github.com>
2025-10-15 09:16:45 +0800

    uodate kinematics

---
+++ kinematicsSDK/FXSpln.h
@@ -0,0 +1,17 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	// c句柄
+	typedef void* FXSpln;
+
+	// C API
+	FXSpln AxisPln_Create();
+	void AxisPln_OnMovL(FXSpln spln, long RobotSerial, double start_pos[6], double end_pos[6], double ref_joints[7], double vel, double acc, double jerk, char* path);
+	void AxisPln_OnMovJ(FXSpln spln, long RobotSerial, double start_joint[7], double end_joint[7], double vel, double acc, double jerk, char* path);
+	void AxisPln_Destroy(FXSpln spln);
+
+#ifdef __cplusplus
+}
+#endif
+
