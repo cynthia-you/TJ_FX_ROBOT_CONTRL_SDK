f35a98e252551609ebd3e078a4fa16cdfef575d0
cynthia-you <37206493+cynthia-you@users.noreply.github.com>
2025-11-14 16:19:21 +0800

    update code: add arm code visualize

--- MARVIN_APP_UBUNTU_WINDOWS/UI_FX2.py
+++ MARVIN_APP_UBUNTU_WINDOWS/UI_FX2.py
@@ -2164,6 +2164,17 @@ class App:
         )
         self.left_state_2.pack(side=tk.LEFT, padx=2)
 
+        self.left_state_3 = tk.Label(
+            self.left_frame,
+            text="错误码:0",
+            bg="#e0e0e0",
+            font=("Arial", 9),
+            padx=2,
+            pady=2,
+            width=10
+        )
+        self.left_state_3.pack(side=tk.LEFT, padx=2)
+
         self.left_data = tk.Label(
             self.left_frame,
             text="J1-J7: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]",
@@ -2229,6 +2240,18 @@ class App:
             width=10
         )
         self.right_state_2.pack(side=tk.LEFT, padx=2)
+
+
+        self.right_state_3 = tk.Label(
+            self.right_frame,
+            text="错误码:0",
+            bg="#e0e0e0",
+            font=("Arial", 9),
+            padx=2,
+            pady=2,
+            width=10
+        )
+        self.right_state_3.pack(side=tk.LEFT, padx=2)
 
         self.right_data = tk.Label(
             self.right_frame,
@@ -2431,9 +2454,11 @@ class App:
         self.left_state_main.config(text=f"状态:{self.result['states'][0]['cur_state']}")
         self.left_state_1.config(text=f"拖动按钮:{self.result['outputs'][0]['tip_di'][0]}")
         self.left_state_2.config(text=f"低速标志:{self.result['outputs'][0]['low_speed_flag'][0]}")
+        self.left_state_3.config(text=f"错误码:{self.result['outputs'][0]['err_code'][0]}")
         self.right_state_main.config(text=f"状态:{self.result['states'][1]['cur_state']}")
         self.right_state_1.config(text=f"拖动按钮:{self.result['outputs'][1]['tip_di'][0]}")
         self.right_state_2.config(text=f"低速标志:{self.result['outputs'][1]['low_speed_flag'][0]}")
+        self.right_state_3.config(text=f"错误码:{self.result['outputs'][1]['err_code'][0]}")
 
         # 根据当前模式获取数据
         key = self.data_keys[self.display_mode]
