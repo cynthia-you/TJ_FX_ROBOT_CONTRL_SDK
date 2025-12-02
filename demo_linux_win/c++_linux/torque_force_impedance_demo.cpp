445bb255d23a680a4112846028491e5560246b4b
cynthia-you <37206493+cynthia-you@users.noreply.github.com>
2025-12-02 15:09:03 +0800

    Refactor torque force impedance demo for clarity

--- demo_linux_win/c++_linux/torque_force_impedance_demo.cpp
+++ demo_linux_win/c++_linux/torque_force_impedance_demo.cpp
@@ -12,12 +12,12 @@
 //     1 初始化订阅数据的结构体
 //     2 查验连接是否成功,失败程序直接退出
 //     3 为了防止伺服有错，先清错
-//     4 设置阻抗参数
-//     5 设置扭矩模式和速度加速度百分比
-//     6 选择阻抗模式
+//     4 设置机器人运动控制模式为力控
+//     5 设置力控参数
+//     6 刷新订阅数据查看设置是否成功
 //     7 订阅数据查看是否设置
-//     8 下发运动点位1
-//     9 订阅查看是否运动到位
+//     8 下发力控指令
+//     9 订阅数据查看是否设置
 //     10 任务完成,释放内存使别的程序或者用户可以连接机器人
 // '''#################################################################
 
@@ -70,29 +70,21 @@ int main()
     OnSetSend();
     usleep(100000);
 
-    //设置力控数据
-    //设置是在Y轴方向有个10N的力一直拽着手臂提起5厘米， 上下拖动手臂试试， 手臂像弹簧一样会回到原来的位置。力控阻抗下更柔顺
+    //设置机器人运动控制模式为力控
+    OnClearSet();
+    OnSetTargetState_A(3) ; //3:torque mode; 1:position mode;
+    OnSetImpType_A(3) ;//type = 1 关节阻抗;type = 2 坐标阻抗;type = 3 力控
+    OnSetSend();
+    usleep(100000);
+
+    //设置力控参数：
+    //设置在Y轴方向有5厘米的调节范围
     int fcType=0; // current only support 0
     double fxDirection[6] = {0, 1, 0, 0, 0, 0}; //前三个方向可调：X，Y，Z ；一次仅可控制一个方向
     double fcCtrlpara[7]={0, 0, 0, 0, 0, 0, 0}; //initial as 0
     double fcAdjLmt=5.0; // 5 厘米
-    double force=10;
     OnClearSet();
     OnSetForceCtrPara_A(fcType, fxDirection,fcCtrlpara,fcAdjLmt);
-    OnSetForceCmd_A(force);
-    OnSetSend();
-    usleep(100000);
-
-    //设置关节的速度和加速度
-    OnClearSet();
-    OnSetJointLmt_A(10, 10) ;
-    OnSetSend();
-    usleep(100000);
-
-    //设置机器人运动控制模式为力控
-    OnClearSet();
-    OnSetTargetState_A(3) ; //3:torque mode; 1:position mode; 
-    OnSetImpType_A(3) ;//type = 1 关节阻抗;type = 2 坐标阻抗;type = 3 力控
     OnSetSend();
     usleep(100000);
 
@@ -101,19 +93,14 @@ int main()
     printf("current state of A arm:%d\n",t.m_State[0].m_CurState);
     printf("cmd state of A arm:%d\n",t.m_State[0].m_CmdState);
     printf("error code of A arms:%d\n",t.m_State[0].m_ERRCode);
-
-    printf("CMD of imdepancd:%d\n",t.m_In[0].m_ImpType);
-    printf("CMD of vel and acc:%d %d\n",t.m_In[0].m_Joint_Vel_Ratio,t.m_In[0].m_Joint_Acc_Ratio);
-
-        printf("CMD of force type=%d\n",t.m_In[0].m_Force_Type);
-    
+    printf("CMD of impedance:%d\n",t.m_In[0].m_ImpType);
+    printf("CMD of force type=%d\n",t.m_In[0].m_Force_Type);
     printf("CMD of force fxDirection=[%lf %lf %lf %lf %lf %lf]\n",t.m_In[0].m_Force_Dir[0],
                                                             t.m_In[0].m_Force_Dir[1],
                                                             t.m_In[0].m_Force_Dir[2],
                                                             t.m_In[0].m_Force_Dir[3],
                                                             t.m_In[0].m_Force_Dir[4],
                                                             t.m_In[0].m_Force_Dir[5]);
-
     printf("CMD of force fcCtrlpara=[%lf %lf %lf %lf %lf %lf %lf]\n",t.m_In[0].m_Force_PIDUL[0],
                                                             t.m_In[0].m_Force_PIDUL[1],
                                                             t.m_In[0].m_Force_PIDUL[2],
@@ -122,33 +109,19 @@ int main()
                                                             t.m_In[0].m_Force_PIDUL[5],
                                                             t.m_In[0].m_Force_PIDUL[6]);
 
-    printf("CMD of force fcAdjLmt=%lf\n",t.m_In[0].m_Force_AdjLmt);
 
-
-    // 设置关节点位
-    double joints[7] = {10,20,30,40,50,30,40};
+    //力控指令
+    //在Y轴方向有个10N的力一直拽着手臂提起5厘米， 上下拖动手臂试试， 手臂像弹簧一样会回到原来的位置。力控阻抗下更柔顺
     OnClearSet();
-    OnSetJointCmdPos_A(joints);
+    double force=10;
+    OnSetForceCmd_A(force);
     OnSetSend();
     usleep(100000);
 
-    //刷新订阅数据查看是否到位
+    //刷新订阅数据查看设置是否成功
     OnGetBuf(&t);
-    printf("CMD joints of arm A :%lf %lf %lf %lf %lf %lf %lf \n",t.m_In[0].m_Joint_CMD_Pos[0],
-                                                                t.m_In[0].m_Joint_CMD_Pos[1],
-                                                                t.m_In[0].m_Joint_CMD_Pos[2],
-                                                                t.m_In[0].m_Joint_CMD_Pos[3],
-                                                                t.m_In[0].m_Joint_CMD_Pos[4],
-                                                                t.m_In[0].m_Joint_CMD_Pos[5],
-                                                                t.m_In[0].m_Joint_CMD_Pos[6]);
-                                                                
-    printf("current joints of arm A :%lf %lf %lf %lf %lf %lf %lf \n",t.m_Out[0].m_FB_Joint_Pos[0],
-                                                                        t.m_Out[0].m_FB_Joint_Pos[1],
-                                                                        t.m_Out[0].m_FB_Joint_Pos[2],
-                                                                        t.m_Out[0].m_FB_Joint_Pos[3],
-                                                                        t.m_Out[0].m_FB_Joint_Pos[4],
-                                                                        t.m_Out[0].m_FB_Joint_Pos[5],
-                                                                        t.m_Out[0].m_FB_Joint_Pos[6]);
+    printf("CMD of force fcAdjLmt=%lf\n",t.m_In[0].m_Force_AdjLmt);
+
     usleep(100000);
 
 
