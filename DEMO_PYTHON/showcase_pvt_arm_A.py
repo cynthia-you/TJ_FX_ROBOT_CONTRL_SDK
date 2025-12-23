217a3737cd765763bbecc2c6e8988ebf7d623771
cynthia-you <37206493+cynthia-you@users.noreply.github.com>
2025-12-23 14:13:30 +0800

    Update showcase_pvt_arm_A.py

--- DEMO_PYTHON/showcase_pvt_arm_A.py
+++ DEMO_PYTHON/showcase_pvt_arm_A.py
@@ -96,7 +96,7 @@ logger.info(f'arm error code:{sub_data["states"][0]["err_code"]}')
 
 '''设置PVT 轨迹本机路径 和PVT号'''
 #linux
-pvt_file=os.path.join(current_path,'/LoadData_ccs_right/LoadData/IdenTraj/LoadIdenTraj_MarvinCCS_Left.fmv')
+pvt_file=current_path+'/LoadData_ccs_right/LoadData/IdenTraj/LoadIdenTraj_MarvinCCS_Left.fmv'
 robot.send_pvt_file('A',pvt_file, 2)
 time.sleep(1)
 
