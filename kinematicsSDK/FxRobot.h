e39d07319dd07b5fd3a1832c6471da47caebcbcd
cynthia-you <37206493+cynthia-you@users.noreply.github.com>
2026-01-21 14:31:34 +0800

    更新源码

--- kinematicsSDK/FxRobot.h
+++ kinematicsSDK/FxRobot.h
@@ -278,18 +278,36 @@ FX_BOOL  FX_Robot_Kine_IK_NSP(FX_INT32L RobotSerial, FX_InvKineSolvePara* solve_
 ////////////////////////////////////////////////////////////////////////////////////////////////
 /////Motion Planning
 FX_BOOL  FX_Robot_PLN_MOVL(FX_INT32L RobotSerial, Vect6 Start_XYZABC, Vect6 End_XYZABC, Vect7 Ref_Joints, FX_DOUBLE Vel, FX_DOUBLE ACC, FX_CHAR* OutPutPath);
-FX_BOOL  FX_Robot_PLN_MOVLA(FX_INT32L RobotSerial, Vect6 Start_XYZABC, Vect6 End_XYZABC, Vect7 Ref_Joints, FX_DOUBLE Vel, FX_DOUBLE ACC, CPointSet* ret_pset);
 FX_BOOL  FX_Robot_PLN_MOVL_KeepJ(FX_INT32L RobotSerial, Vect7 startjoints, Vect7 stopjoints, FX_DOUBLE vel, FX_DOUBLE acc,FX_CHAR* OutPutPath);
-FX_BOOL  FX_Robot_PLN_MOVL_KeepJA(FX_INT32L RobotSerial, Vect7 startjoints, Vect7 stopjoints, FX_DOUBLE vel, FX_DOUBLE acc,CPointSet* ret_pset);
-
 ////Parameters Identification
 FX_INT32  FX_Robot_Iden_LoadDyn(FX_INT32 Type,FX_CHAR* path,FX_DOUBLE* mass, Vect3 mr, Vect6 I);
-
 ////////////////////////////////////////////////////////////////////////////////////////////////
 FX_VOID FX_XYZABC2Matrix4DEG(FX_DOUBLE xyzabc[6], FX_DOUBLE m[4][4]);
 FX_BOOL FX_Matrix42XYZABCDEG(FX_DOUBLE m[4][4],FX_DOUBLE xyzabc[6]);
+
+
+// CPointSet的C风格包装接口
+void* FX_CPointSet_Create();
+void FX_CPointSet_Destroy(void* pset);
+FX_BOOL FX_CPointSet_OnInit(void* pset, FX_INT32L ptype);
+FX_INT32L FX_CPointSet_OnGetPointNum(void* pset);
+FX_DOUBLE* FX_CPointSet_OnGetPoint(void* pset, FX_INT32L pos);
+FX_BOOL FX_CPointSet_OnSetPoint(void* pset, FX_DOUBLE point_value[]);
+
+// 使用C风格接口的MOVLA函数
+FX_BOOL FX_Robot_PLN_MOVLA_C(FX_INT32L RobotSerial, Vect6 Start_XYZABC, Vect6 End_XYZABC,
+                            Vect7 Ref_Joints, FX_DOUBLE Vel, FX_DOUBLE ACC, void* ret_pset);
+FX_BOOL  FX_Robot_PLN_MOVL_KeepJA_C(FX_INT32L RobotSerial, Vect7 startjoints, Vect7 stopjoints,
+                            FX_DOUBLE vel, FX_DOUBLE acc,void* ret_pset);
+
+
 #ifdef __cplusplus
 }
+// C++ only functions
+FX_BOOL FX_Robot_PLN_MOVLA(FX_INT32L RobotSerial, Vect6 Start_XYZABC, Vect6 End_XYZABC,
+                          Vect7 Ref_Joints, FX_DOUBLE Vel, FX_DOUBLE ACC, CPointSet* ret_pset);
+FX_BOOL  FX_Robot_PLN_MOVL_KeepJA(FX_INT32L RobotSerial, Vect7 startjoints, Vect7 stopjoints,
+                            FX_DOUBLE vel, FX_DOUBLE acc,CPointSet* ret_pset);
 #endif
 
 #endif
