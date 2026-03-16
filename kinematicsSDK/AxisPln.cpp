7e2403026c0777643a843271b29f13b993995502
Ranness <58969475+ranness@users.noreply.github.com>
2026-03-16 09:42:20 +0800

    Update AxisPln.cpp
    修改MOVL接口中逆运动学选解策略至各关节欧式距离最小

--- kinematicsSDK/AxisPln.cpp
+++ kinematicsSDK/AxisPln.cpp
@@ -1090,7 +1090,6 @@ bool CAxisPln::OnMovL(long RobotSetial, double ref_joints[7], double start_pos[6
 	////////////////////InvKine//////////////
 	FX_InvKineSolvePara sp;
 
-	sp.m_Input_IK_ZSPType = 1;
 	for (i = 0; i < 6; i++)
 	{
 		sp.m_Input_IK_ZSPPara[i] = 0;
